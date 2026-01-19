423350d0c8c0cb9c4d79c709045313e9f50fbb43
hisana-shah <hisananazeer1@gmail.com>
2026-01-19 13:07:07 -0500

    sign in/signup form,login page completed

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
+import popcorn from '../../public/popcorn.png'
 
 
 const Body = () => {
   return (
     <div>
-       <img src="https://assets.nflxext.com/ffe/siteui/vlv3/797df41b-1129-4496-beb3-6fc2f29c59d3/web/CA-en-20260112-TRIFECTA-perspective_a8b0e8b6-de03-489d-91c1-31028f8ede3c_large.jpg"/>
-      
+       
     </div>
   )
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,27 @@
+"use client"
 import Header from "../components/Header";
+import { useState } from "react";
 
 export default function Page() {
+    const [ showSignUp, setShowSignUp ] = useState(false)
+
+    const toggleSignUpForm = () => {
+        setShowSignUp(!showSignUp);
+    };
+
   return (
     <div className="min-h-screen bg-black text-white">
       <Header />
 
-      <form className="w-full max-w-lg mx-auto mt-20 rounded bg-black/75 p-40 text-white">
-        <h1 className="mb-6 py-4 text-3xl font-semibold">Sign In</h1>
+      <form className="w-full max-w-md mx-auto mt-20 rounded bg-black/75 shadow:lg p-8 md:p-10 text-white">
+        <h1 className="mb-6 pt-20  text-center text-3xl font-semibold">{showSignUp? "Sign Up": "Sign In"}</h1>
 
+        {showSignUp &&(
+        <input
+          type="text"
+          placeholder="Full Name"
+          className="w-full mb-4 h-12 rounded bg-[#333] px-4 placeholder-gray-400 focus:outline-none"
+        /> )}
         <input
           type="text"
           placeholder="Email address"
@@ -21,9 +35,10 @@ export default function Page() {
         />
 
         <button className="w-full rounded bg-orange-500 py-3 text-lg font-semibold hover:bg-red-700 transition">
-          Sign In
+          {showSignUp? "Sign Up": "Sign In"}
         </button>
       </form>
+      <p className="cursor-pointer p-10 mx-20 " onClick={toggleSignUpForm}>{showSignUp?"New to nextWatch? Sign Up now":"Already Registered? Sign In" }</p>
     </div>
   );
-}
+    }
